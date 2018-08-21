20d66858eaa628c59354cca353e56c5125520e2d
susana <susana.zv@gmail.com>
2018-08-21 18:45:06 -0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :students
+  get 'students/index'
+
+  root 'students#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
